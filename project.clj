c0d4f6014ed61d1504c9b06fddfcc2e660c99ef7
Anders Sundman <anders@4zm.org>
2015-02-12 10:21:48 +0100

    Phonegap init code and build

--- project.clj
+++ project.clj
@@ -33,12 +33,12 @@
                                         :externs       ["react/externs/react.js"]
                                         :optimizations :none
                                         :pretty-print  true}}
-                       :phone {:source-paths ["src/cljs"]
+                       :phone {:source-paths ["src/cljs", "phone/cljs"]
                                :compiler {:output-to     "phone/www/js/app.gen.js"
                                           :preamble      ["react/react.min.js"]
                                           :externs       ["react/externs/react.js"]
-                                          :optimizations :simple
-                                          :pretty-print  true}}}}
+                                          :optimizations :advanced
+                                          :pretty-print  false}}}}
 
   :profiles {:dev {:repl-options {:init-ns classwar.server
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
