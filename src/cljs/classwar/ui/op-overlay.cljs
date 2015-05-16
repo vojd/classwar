c364f9692276fac5d4b9c5bbca6443bcf8c11048
Mathias Tervo <mathias.tervo@gmail.com>
2015-05-16 16:27:42 +0200

    Movable progress bar

--- src/cljs/classwar/ui/op-overlay.cljs
+++ src/cljs/classwar/ui/op-overlay.cljs
@@ -5,14 +5,17 @@
 
 (defn operation-view [game grid-to-px-fn operation]
   (let [[gx gy :as grid-pos] (:pos operation)
-        [x y] (grid-to-px-fn grid-pos)]
+        [x y] (grid-to-px-fn grid-pos)
+        progress (* 100 (/  (- (:time game) (:start operation)) (:duration operation)))]
+    (.log js/console "pr" progress)
+    (.log js/console (str progress "%"))
     (dom/div #js {:className "operation-overlay"
                   :style #js {:top x
                               :left y
                               :background "url('img/activist.svg')"}}
              (dom/div #js {:className "operation-overlay-progress-bar"}
                       (dom/div #js {:className "operation-overlay-progress-bar-overlay"
-                                    :width "50%"})))))
+                                    :style #js {:width (str progress "%")}})))))
 
 (defn operations-view [game owner]
   (reify om/IRenderState
